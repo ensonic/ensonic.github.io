a2b6e23d50ff96c00a269c18dffece742b72fa44
Stefan Sauer <ensonic@users.sf.net>
2020-05-31 16:39:58 +0200

    apps/skel: add basic nav, manifest and icon

---
+++ apps/skel/index.js
@@ -0,0 +1,18 @@
+function initUI() {
+  document.getElementById("defaultPage").click();
+}
+
+function openPage(evt, pageName) {
+  var i, tabcontent, tablinks;
+  pages = document.getElementsByClassName("pagecontent");
+  for (i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
+  links = document.getElementsByClassName("pagelinks");
+  for (i = 0; i < links.length; i++) {
+    links[i].className = links[i].className.replace(" active", "");
+  }
+  document.getElementById(pageName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
