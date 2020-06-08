3889c6641dc1e95c1726cd6cf764f840d7765729
Stefan Sauer <ensonic@users.sf.net>
2020-06-08 17:47:27 +0200

    apps: change service workser to cache as needed

--- apps/keys/service-worker.js
+++ apps/keys/service-worker.js
@@ -45,11 +45,8 @@ self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-      })
+      caches.match(event.request)
+        .then(cachedResponse => cachedResponse || fetch(event.request))
     );
   }
 });
